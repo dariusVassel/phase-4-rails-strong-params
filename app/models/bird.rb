d37362b76f65cc6c194a0978997ba90e0e2f35b5
+ <+>
2022-04-27 11:36:52 -0400

    Complete

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,17 @@
 class Bird < ApplicationRecord
+    wrap_parameters format: []
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
